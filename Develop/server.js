27b1d4b83379c082539d88eb6e3701bedb0f997e
Sanjay Gonsalves <snjgonsalves@gmail.com>
2020-12-15 11:05:15 -0800

    added notes html

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
 const app = express();
-
-
+const path = require('path')
+app.use(express.json());
 app.get('/', (req,res) => {
     res.send("Hello WOrld, FIghting from Nodemon and express, Hop i can do this right");
 })
 
-
+app.get('/public/notes.html', (req,res)=>{
+    res.sendFile(path.join(___dirname, 'notes.html'))
+})
 
 
 
