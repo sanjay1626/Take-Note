91110772c0ce261dff5f4cc5f5915f4822042ccb
Sanjay Gonsalves <snjgonsalves@gmail.com>
2020-12-19 12:39:45 -0800

    Ready for Heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 //make public folder accessible
 app.use(express.static(path.join(__dirname,"/public")));
-
+app.use(express.static(path.join(__dirname,"/db")));
 
 require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
