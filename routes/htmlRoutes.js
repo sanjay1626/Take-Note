d7c5cef69f0b20ebd381b9b9c3701f829295fa0d
Sanjay Gonsalves <snjgonsalves@gmail.com>
2020-12-19 09:16:28 -0800

    Updated routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,26 +9,26 @@ var path = require("path");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
-  app.get('/', (req,res) => {
-    res.send("Hello WoRld, Fighting from Nodemon and express, Hope i can do this right!!");
-
-})
-  app.get("/notes", function(req, res) {
+  app.get('/', (req, res) => {
+    //res.send("Hello WoRld, Fighting from Nodemon and express, Hope i can do this right!!");
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  })
+  app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
-  app.get("/index", function(req, res) {
+  app.get("/index", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   //// If no matching route is found default to home
   //giving an err it wont allow to display /api/notes when this is used??
-  //app.get("*", function(req, res) {
-  //  res.sendFile(path.join(__dirname, "../public/index.html"));
-  //});
+ //app.get("/*", function(req, res) {
+ //  res.sendFile(path.join(__dirname, "../public/index.html"));
+ //});
 };
